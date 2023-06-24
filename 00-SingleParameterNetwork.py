d243827ee9962cc5a0f3a75c868c20a9c44834d7
scpmx <13601394+scpmx@users.noreply.github.com>
2023-06-24 00:42:26 -0400

    Initial Commit

---
+++ 00-SingleParameterNetwork.py
@@ -0,0 +1,72 @@
+import random
+
+class SingleParameterNetwork:
+
+    def __init__(self, w: float) -> None:
+        self.w = w
+    
+    def FeedForward(self, i: float) -> float:
+        # <Exercise>
+        # Apply the network on the input i
+        return self.w * i
+
+    def ApplyGradient(self, grad: float, learningRate: float) -> None:
+        # <Exercise>
+        # Apply the gradient to the param proportional to the learning rate
+        self.w += grad * learningRate
+
+    def GetParams(self) -> float:
+        return self.w
+    
+    def Show(self) -> None:
+        print("w:", str(self.w))
+
+class SingleParameterNetworkTrainer:
+
+    def __init__(self, ti: float, ta: float) -> None:
+        self.ti = ti
+        self.ta = ta
+    
+    def ComputeNetworkCost(self, spn: SingleParameterNetwork) -> float:
+        # <Exercise>
+        # Use mean squared error to calculate a cost of the network relative
+        # to the training data 
+        actual = spn.FeedForward(self.ti)
+        mean = actual - self.ta
+        return pow(mean, 2)
+
+    def ComputeGradient(self, spn: SingleParameterNetwork) -> float:
+        # <Exercise>
+        # Use the chain rule to find dC/dw 
+        return (2 * spn.GetParams() * pow(self.ti, 2)) - (2 * self.ti * self.ta)
+
+    def Fit(self, spn: SingleParameterNetwork, learningRate: float) -> None:
+        # <Exercise>
+        # Write a basic gradient descent algorithm 
+        cost = self.ComputeNetworkCost(spn)
+        while cost > 0.0001:
+            grad = self.ComputeGradient(spn)
+            spn.ApplyGradient(-1 * grad, learningRate)
+            spn.Show() # See the cost decrease :D
+            cost = self.ComputeNetworkCost(spn)
+
+# Randomly initialize parameter for network
+w = random.uniform(0, 1)
+spn = SingleParameterNetwork(w)
+
+# Randomly initialize training input and output
+ti = random.uniform(0.1, 1)
+ta = random.uniform(0, 1)
+
+# Print initial values to screen
+print("ti:", str(ti), "ta:", str(ta))
+spn.Show()
+
+# Initialize a network trainer and use it to fit our single parameter
+# network to our training data with a learning rate of 0.1
+spnt = SingleParameterNetworkTrainer(ti, ta)
+spnt.Fit(spn, 0.1)
+
+# Print final network cost
+finalCost = spnt.ComputeNetworkCost(spn)
+print("final cost:", str(finalCost))
